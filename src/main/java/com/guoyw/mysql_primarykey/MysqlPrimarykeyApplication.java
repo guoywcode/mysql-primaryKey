9a432e433de4545b38a855b9ef97802389da4aab
guoywcode <943364123@qq.com>
2019-12-19 15:48:30 +0800

    feat: 分布式自增ID 16位uuid

---
+++ src/main/java/com/guoyw/mysql_primarykey/MysqlPrimarykeyApplication.java
@@ -0,0 +1,13 @@
+package com.guoyw.mysql_primarykey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlPrimarykeyApplication{
+
+  public static void main(String[] args){
+    SpringApplication.run(MysqlPrimarykeyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/guoyw/mysql_primarykey/MysqlPrimarykeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoyw.mysql_primarykey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MysqlPrimarykeyApplicationTests{
+
+  @Test
+ public void contextLoads(){
+  }
+
+}
